826c17dabf9bdd2590ce427a38e799af9addb016
Duan Jinjin <jjduan@best-inc.com>
2018-01-23 15:52:36 +0800

    ch03/classfile/

---
+++ ch03/classfile/cp_name_and_type.go
@@ -0,0 +1,20 @@
+package classfile
+
+//ref  https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.4.6
+
+/*
+CONSTANT_NameAndType_info {
+    u1 tag;
+    u2 name_index;
+    u2 descriptor_index;
+}
+*/
+type ConstantNameAndTypeInfo struct {
+	nameIndex       uint16
+	descriptorIndex uint16
+}
+
+func (self *ConstantNameAndTypeInfo) readInfo(reader *ClassReader) {
+	self.nameIndex = reader.readUint16()
+	self.descriptorIndex = reader.readUint16()
+}
